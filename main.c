17d4ccaa77d838e7f6f4bfe12b9dfb554d991be5
FIDEL CHITTETTUKALAM BITTO <156352152+FidelChittettukalamBitto@users.noreply.github.com>
2024-04-13 23:17:14 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,45 @@
-/*
- * Name:Fidel Chittettukalam Bitto
- Student Id: D23125253
- */
+#include <stdio.h>
+#include <stdlib.h>
 
-#ifndef RIJNDAEL_H
-#define RIJNDAEL_H
+#include "rijndael.h"
 
-#define BLOCK_ACCESS(block, row, col) (block[(row * 4) + col])
-#define BLOCK_SIZE 16
+void print_128bit_block(unsigned char *block) {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      unsigned char value = BLOCK_ACCESS(block, i, j);
 
-/*
- * These should be the main encrypt/decrypt functions (i.e. the main
- * entry point to the library for programmes hoping to use it to
- * encrypt or decrypt data)
- */
-unsigned char *aes_encrypt_block(unsigned char *plaintext, unsigned char *key);
-unsigned char *aes_decrypt_block(unsigned char *ciphertext, unsigned char *key);
+      // Print spaces before small numbers to ensure that everything is aligned
+      // and looks nice
+      if (value < 10) printf(" ");
 
-#endif
+      if (value < 100) printf(" ");
+
+      printf("%d", value);
+    }
+    printf("\n");
+  }
+}
+
+int main() {
+  unsigned char plaintext[16] = {1, 2,  3,  4,  5,  6,  7,  8,
+                                 9, 10, 11, 12, 13, 14, 15, 16};
+  unsigned char key[16] = {50, 20, 46, 86, 67, 9, 70, 27,
+                           75, 17, 51, 17, 4,  8, 6,  99};
+
+  unsigned char *ciphertext = aes_encrypt_block(plaintext, key);
+  unsigned char *recovered_plaintext = aes_decrypt_block(ciphertext, key);
+
+  printf("############ ORIGINAL PLAINTEXT ###########\n");
+  print_128bit_block(plaintext);
+
+  printf("\n\n################ CIPHERTEXT ###############\n");
+  print_128bit_block(ciphertext);
+
+  printf("\n\n########### RECOVERED PLAINTEXT ###########\n");
+  print_128bit_block(recovered_plaintext);
+
+  free(ciphertext);
+  free(recovered_plaintext);
+
+  return 0;
+}
