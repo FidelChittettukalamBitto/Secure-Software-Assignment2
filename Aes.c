ee318487b2e323103243a10cf70c7a269b3b7255
FIDEL CHITTETTUKALAM BITTO <156352152+FidelChittettukalamBitto@users.noreply.github.com>
2024-04-13 18:37:06 +0200

    last Aes.c

--- Aes.c
+++ Aes.c
@@ -215,6 +215,156 @@ void _generateSubkeys() {
     printSubKeys();
 }
 
+void _initFromFile(int size, uint8_t matrix[size][size], char * file_name) {
+    FILE *file;
+    if ((file = fopen(file_name, "r")) == NULL){
+        printf("Error! opening file");
+        exit(1);
+    }
+
+    // variable to read value
+    unsigned int input_var;
+    if (size == STATE_SIZE) {  // key and plaintext
+        for(int c=0; c<size; c++) {
+            for(int r=0; r<size; r++) {
+                fscanf(file, "%x", &input_var);
+                // Each value should be 1 byte
+                assert(input_var<=255);
+                matrix[r][c] = (uint8_t)input_var;
+            }
+        }
+    }    
+    else {  // sbox
+        for(int r=0; r<size; r++) {
+            for(int c=0; c<size; c++) {
+                fscanf(file, "%x", &input_var);
+                // Each value should be 1 byte
+                assert(input_var<=255);
+                matrix[r][c] = (uint8_t)input_var;
+            }
+        }
+    }
+    fclose(file);
+}
+
+
+void _addRoundKey(int round) {
+    for(int r=0; r<STATE_SIZE; r++) {
+        for (int c=0; c<STATE_SIZE; c++) {
+            state[r][c] ^= sub_keys[round][r][c];
+        }
+    }
+}
+
+
+void _shiftRows() {
+    // shift row 1 by 1 byte
+    _helperShiftRow(1);
+    // shift row 2 by 2 byte
+    _helperShiftRow(2);
+    _helperShiftRow(2);
+    // shift row 3 by 3 byte
+    _helperShiftRow(3);
+    _helperShiftRow(3);
+    _helperShiftRow(3);
+}
+
+
+void _invShiftRows() {
+    // shift row 1 by 3 bytes
+    _helperShiftRow(1);
+    _helperShiftRow(1);
+    _helperShiftRow(1);
+    // shift row 2 by 2 bytes
+    _helperShiftRow(2);
+    _helperShiftRow(2);
+    // shift row 3 by 1 byte
+    _helperShiftRow(3);
+}
+
+
+void _helperShiftRow(int row) {
+    // shift row by 1 byte
+    uint8_t temp = state[row][0];
+    state[row][0] = state[row][1];
+    state[row][1] = state[row][2];
+    state[row][2] = state[row][3];
+    state[row][3] = temp;
+}
+
+
+void _subByte(uint8_t sbox_to_use[SBOX_SIZE][SBOX_SIZE]) {
+    for(int r=0; r<STATE_SIZE; r++) {
+        for (int c=0; c<STATE_SIZE; c++) {
+            state[r][c] = SBOX_LOOKUP(sbox_to_use, state[r][c]);
+        }
+    }
+}
+
+
+void _mixColumns() {
+    uint8_t temp1, temp2, temp3, temp4;
+    // Multiply each column of state with diffuision matrix
+    for (int c=0; c<STATE_SIZE; c++) {
+        temp1 = _gmult(2, state[0][c]) ^ _gmult(3, state[1][c]) ^ _gmult(1, state[2][c]) ^ _gmult(1, state[3][c]);
+        temp2 = _gmult(1, state[0][c]) ^ _gmult(2, state[1][c]) ^ _gmult(3, state[2][c]) ^ _gmult(1, state[3][c]);
+        temp3 = _gmult(1, state[0][c]) ^ _gmult(1, state[1][c]) ^ _gmult(2, state[2][c]) ^ _gmult(3, state[3][c]);
+        temp4 = _gmult(3, state[0][c]) ^ _gmult(1, state[1][c]) ^ _gmult(1, state[2][c]) ^ _gmult(2, state[3][c]);
+
+        state[0][c] = temp1;
+        state[1][c] = temp2;
+        state[2][c] = temp3;
+        state[3][c] = temp4;
+    }
+}
+
+
+void _invMixColumns() {
+    uint8_t temp1, temp2, temp3, temp4;
+    // Multiply each column of state with diffuision matrix
+    for (int c=0; c<STATE_SIZE; c++) {
+        temp1 = _gmult(0x0e, state[0][c]) ^ _gmult(0x0b, state[1][c]) ^ _gmult(0x0d, state[2][c]) ^ _gmult(0x09, state[3][c]);
+        temp2 = _gmult(0x09, state[0][c]) ^ _gmult(0x0e, state[1][c]) ^ _gmult(0x0b, state[2][c]) ^ _gmult(0x0d, state[3][c]);
+        temp3 = _gmult(0x0d, state[0][c]) ^ _gmult(0x09, state[1][c]) ^ _gmult(0x0e, state[2][c]) ^ _gmult(0x0b, state[3][c]);
+        temp4 = _gmult(0x0b, state[0][c]) ^ _gmult(0x0d, state[1][c]) ^ _gmult(0x09, state[2][c]) ^ _gmult(0x0e, state[3][c]);
+
+        // values are stored in temp so values don't change in mid-calculation
+        state[0][c] = temp1;
+        state[1][c] = temp2;
+        state[2][c] = temp3;
+        state[3][c] = temp4;
+    }
+}
+
+
+
+uint8_t _gmult(uint8_t a, uint8_t b) {
+	uint8_t p = 0; /* the product of the multiplication */
+	while (a && b) {
+            if (b & 1) /* if b is odd, then add the corresponding a to p (final product = sum of all a's corresponding to odd b's) */
+                p ^= a; /* since we're in GF(2^m), addition is an XOR */
+
+            if (a & 0x80) /* GF modulo: if a >= 128, then it will overflow when shifted left, so reduce */
+                a = (a << 1) ^ 0x11b; /* XOR with the primitive polynomial x^8 + x^4 + x^3 + x + 1 (0b1_0001_1011) – you can change it but it must be irreducible */
+            else
+                a <<= 1; /* equivalent to a*2 */
+            b >>= 1; /* equivalent to b // 2 */
+	}
+	return p;
+}
+
+
+void _printMatrix(int size, uint8_t matrix[size][size]) {
+    printf("\n");
+    for(int r=0; r<size; r++) {
+        for(int c=0; c<size; c++) {
+            printf("%02x ", matrix[r][c]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
 
 
 
