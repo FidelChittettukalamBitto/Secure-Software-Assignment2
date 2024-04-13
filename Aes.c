abf7ef81b5fd6593abe46ba14e773c65b352fbaa
FIDEL CHITTETTUKALAM BITTO <156352152+FidelChittettukalamBitto@users.noreply.github.com>
2024-04-13 16:43:51 +0200

    added headers Aes.c

--- Aes.c
+++ Aes.c
@@ -13,3 +13,18 @@ uint8_t static sbox[SBOX_SIZE][SBOX_SIZE];
 uint8_t static inv_sbox[SBOX_SIZE][SBOX_SIZE];
 uint8_t static sub_keys [MAX_ROUNDS][KEY_SIZE][KEY_SIZE];
 int initialize_called;
+// Private Headers
+void _generateSubkeys();
+void _initFromFile(int size, uint8_t matrix[size][size], char * file_name);
+void _addRoundKey(int round);
+void _shiftRows();
+void _invShiftRows();
+void _helperShiftRow(int row);
+void _subByte(uint8_t sbox_to_use[SBOX_SIZE][SBOX_SIZE]);
+void _mixColumns();
+void _invMixColumns();
+
+uint8_t _gmult(uint8_t a, uint8_t b);
+void _printMatrix(int size, uint8_t matrix[size][size]);
+
+
