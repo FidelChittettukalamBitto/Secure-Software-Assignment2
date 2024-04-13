38e3ac6ba200c5551de427994a91aa4156f75424
FIDEL CHITTETTUKALAM BITTO <156352152+FidelChittettukalamBitto@users.noreply.github.com>
2024-04-13 19:11:56 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,45 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "aes.h"
 
-#include "rijndael.h"
 
-void print_128bit_block(unsigned char *block) {
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
-      unsigned char value = BLOCK_ACCESS(block, i, j);
-
-      // Print spaces before small numbers to ensure that everything is aligned
-      // and looks nice
-      if (value < 10) printf(" ");
-
-      if (value < 100) printf(" ");
-
-      printf("%d", value);
+int main(int argc, char *argv[])
+{
+    if(argc < 5) {
+        printf("ERROR: expecting 4 paths \"key_file, plaintext_file, sbox_file, inv_sbox_file\"\n");
+        exit(-1);
     }
-    printf("\n");
-  }
-}
 
-int main() {
-  unsigned char plaintext[16] = {1, 2,  3,  4,  5,  6,  7,  8,
-                                 9, 10, 11, 12, 13, 14, 15, 16};
-  unsigned char key[16] = {50, 20, 46, 86, 67, 9, 70, 27,
-                           75, 17, 51, 17, 4,  8, 6,  99};
+    char* key_file = NULL, *state_file = NULL, *sbox_file = NULL, *inv_sbox_file = NULL;
+    key_file = argv[1];
+    state_file = argv[2];
+    sbox_file = argv[3];
+    inv_sbox_file = argv[4];
 
-  unsigned char *ciphertext = aes_encrypt_block(plaintext, key);
-  unsigned char *recovered_plaintext = aes_decrypt_block(ciphertext, key);
+    initialize(key_file, state_file, sbox_file, inv_sbox_file);
+    encrypt();
+    decrypt();
+    assertStateIsOrigMesg();
 
-  printf("############ ORIGINAL PLAINTEXT ###########\n");
-  print_128bit_block(plaintext);
-
-  printf("\n\n################ CIPHERTEXT ###############\n");
-  print_128bit_block(ciphertext);
-
-  printf("\n\n########### RECOVERED PLAINTEXT ###########\n");
-  print_128bit_block(recovered_plaintext);
-
-  free(ciphertext);
-  free(recovered_plaintext);
-
-  return 0;
+    printf("\nProgram ended Successfully\n");
+    return 0;
 }
